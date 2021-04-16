30d3a4b39c39cc63e087debb6e66c2d73eb7c6e0
Brock Ellis <brock@sharproot.com>
2021-04-15 20:51:07 -0500

    working geolocation watcher

--- src/app/bounty-active/bounty-active.page.ts
+++ src/app/bounty-active/bounty-active.page.ts
@@ -23,71 +23,126 @@ export class BountyActivePage implements OnInit {
     latitude: 41.451861710348645,
     longitude: -96.48415217309466
   }
+  private watcherId: string;
+  private userRawPosition: any = {
+    coords: {}
+  }
+  private userMarker: any;
+  private bountyCircle: any;
+  private userLocationObject: any;
 
   constructor() { }
 
   ngOnInit() {
-    this.loadMap();
+    this.loadMap().then( () => {
+      console.log('shit got resolved');
+      this.watchLocation();
+    })
   }
 
   loadMap() {
-
-    this.userLocation = Geolocation.getCurrentPosition().then( position => {
-      console.log("Position: ", position);
-
+    return Geolocation.getCurrentPosition().then(position => {
       // the users current position
-      const userLocationObject = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      this.userLocationObject = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
 
       const mapOptions = {
         zoom: 12,
-        center: userLocationObject
+        center: this.userLocationObject
       }
-
+      
       this.map = new google.maps.Map(
         this.mapElement.nativeElement, // element on the page
         mapOptions // customization options
       )
 
-      // this marker is the users current position
-      const userMarker = new google.maps.Marker({
-        map: this.map,
-        position: userLocationObject,
-        animation: google.maps.Animation.DROP 
-      })
+      return Promise.resolve()
+    })
+    
+  }
 
-      const bountyLocationObject = new google.maps.LatLng(this.bountyLocation.latitude, this.bountyLocation.longitude)
+  watchLocation() {
+    console.log('Watching location');
 
-      const distanceToBounty = Math.round(
-        google.maps.geometry.spherical.computeDistanceBetween(userLocationObject, bountyLocationObject)
-      )
+    const positionOptions = {
+      maximumAge: 0,
+      enableHighAccuracy: false
+    }
 
-      console.log('Distance to the bounty:', distanceToBounty);
+    this.watcherId = Geolocation.watchPosition(positionOptions, (position) => {
+      
+      console.log('Got watched position', position);
 
-      let radius
-      // calculate size of the radius
-      if (distanceToBounty > 1000) {
-        radius = 500
-      } else if (distanceToBounty <= 1000 && distanceToBounty > 500) {
-        radius = 300
-      } else if (distanceToBounty <= 500 && distanceToBounty > 100) {
-        radius = 100
-      } else {
-        radius = 25
-      }
+      if(
+        !position ||
+        !position.coords ||
+        this.userRawPosition.coords.latitude === position.coords.latitude &&
+        this.userRawPosition.coords.longitude === position.coords.longitude
+      ) {
+        console.log('Same location, skipping rest of function', this.userRawPosition, position);
+        return false;
+        }
 
-      const bountyCircle = new google.maps.Circle({
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
-        map: this.map,
-        center: bountyLocationObject,
-        radius
-      });
+      // save for the next time this runs
+      this.userRawPosition = position;
 
+      console.log("Position: ", position);
+
+      this.updateUserLocation();
+      this.updateBountyCircle();
+  
     })
 
+  }
+
+  updateUserLocation() {
+    // the users current position
+    this.userLocationObject = new google.maps.LatLng(
+      this.userRawPosition.coords.latitude,
+      this.userRawPosition.coords.longitude
+    )
+
+    // this marker is the users current position
+    if(this.userMarker) this.userMarker.setMap(null);
+    this.userMarker = new google.maps.Marker({
+      map: this.map,
+      position: this.userLocationObject,
+      animation: google.maps.Animation.DROP 
+    })
+  }
+
+  updateBountyCircle() {
+    const bountyLocationObject = new google.maps.LatLng(this.bountyLocation.latitude, this.bountyLocation.longitude)
+
+    const distanceToBounty = Math.round(
+      google.maps.geometry.spherical.computeDistanceBetween(this.userLocationObject, bountyLocationObject)
+    )
+
+    console.log('Distance to the bounty:', distanceToBounty);
+
+    let radius
+    // calculate size of the radius
+    if (distanceToBounty > 1000) {
+      radius = 500
+    } else if (distanceToBounty <= 1000 && distanceToBounty > 500) {
+      radius = 300
+    } else if (distanceToBounty <= 500 && distanceToBounty > 100) {
+      radius = 100
+    } else {
+      radius = 25
+    }
+
+    if(this.bountyCircle) this.bountyCircle.setMap(null);
+
+    this.bountyCircle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: this.map,
+      center: bountyLocationObject,
+      radius
+    });
   }
 
   // TODO - animate the initial map with this method
